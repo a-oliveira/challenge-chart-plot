693a375398d18d6c76e3a93299d8073cae430067
a-oliveira <dessaschweitzer@gmail.com>
2019-04-06 15:01:15 -0300

    main

---
+++ index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React    from 'react';
+import Boxes from './layout.js';
+
+/* It defines the Boxes component must be rendered on root.*/
+
+ReactDOM.render(
+    <Boxes />,
+    document.getElementById('root')
+);
